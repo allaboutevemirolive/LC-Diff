7c06ffdbccd9ca6df168ef8208e9418a032143bc
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-26 01:17:46 +0200

    1/26/2023

---
+++ LC Difficulty/1. Easy/27. Remove Element/A0027.java
@@ -0,0 +1,8 @@
+public class A0027 {
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 2, 3};
+        int nums2 = 3;
+
+        System.out.println(B0027.removeElement(nums1, nums2));
+    }
+}
