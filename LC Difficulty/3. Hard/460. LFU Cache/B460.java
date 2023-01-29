4c97a7181124dd12b4529983a52f7164a82d6948
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-29 09:23:12 +0200

    1/29/2023

--- LC Difficulty/3. Hard/460. LFU Cache/B460.java
+++ LC Difficulty/3. Hard/460. LFU Cache/B460.java
@@ -1,157 +1,49 @@
-class B460 {
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 
-    /*
-     * Time: O(n)
-     * Space: O(n)
-     */
+public class B460 {
+    private Map<Integer, Integer> vals; // map to store the key-value pairs
+    private Map<Integer, Integer> counts; // map to store the use count for each key
+    private Map<Integer, LinkedHashSet<Integer>> lists; // map to store the keys with the same use count in a LinkedHashSet, in order of insertion
+    private int cap; // capacity of the cache
+    private int min = -1; // the minimum use count among all keys in the cache
 
-    /**
-     * Your LFUCache object will be instantiated and called as such:
-     * LFUCache obj = new LFUCache(capacity);
-     * int param_1 = obj.get(key);
-     * obj.put(key,value);
-     */
-
-    private int capacity; // maximum number of items that can be stored in the cache
-    private int size; // current number of items stored in the cache
-    private int leastCounter; // least frequency count of any item in the cache
-    private Node[] cache; // array to store the items in the cache, indexed by key
-    private Node[] counterMap; // array to store the linked list of items for each frequency count
-    private Node[] tails; // array to store the tail of each linked list, indexed by frequency count
-
-    // Initializes the object with the capacity of the data structure.
     public void LFUCache(int capacity) {
-        this.capacity = capacity;
-        this.size = 0;
-        this.cache = new Node[100_001]; // create an array to store the items in the cache
-        this.counterMap = new Node[100_001]; // create an array to store the linked lists of items for each frequency count
-        this.tails = new Node[100_001]; // create an array to store the tail of each linked list
+        cap = capacity;
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>()); // initialize the LinkedHashSet for the use count 1
     }
 
-    // Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
     public int get(int key) {
-        int result = -1; // initialize result to -1, meaning the key is not in the cache
-
-        if (cache[key] != null) { // if the key is in the cache
-            incCounter(cache[key]); // increase the frequency count of the item
-            result = cache[key].value; // return the value of the item
-        }
-
-        return result; // return the result
+        if (!vals.containsKey(key)) return -1; // key not found in the cache
+        int count = counts.get(key);
+        counts.put(key, count + 1); // increment the use count for the key
+        lists.get(count).remove(key);
+        if (count == min && lists.get(count).size() == 0) min++; // if the key with the minimum use count is removed, update the minimum use count
+        if (!lists.containsKey(count + 1)) lists.put(count + 1, new LinkedHashSet<>());
+        lists.get(count + 1).add(key); // add the key to the LinkedHashSet with the new use count
+        return vals.get(key);
     }
 
-    // Update the value of the key if present, or inserts the key if not already present.
     public void put(int key, int value) {
-
-        if (cache[key] != null) { // if the key is already in the cache
-            incCounter(cache[key]); // increase the frequency count of the item
-            cache[key].value = value; // update the value of the item
-            cache[key].key = key; // update the key of the item
-        } else if (capacity != 0) { // if the key is not in the cache and the cache is not full
-
-            if (size == capacity) { // if the cache is full
-                evict(); // remove the least frequently used item from the cache
-            }
-
-            Node node = new Node(); // create a new item
-            node.counter = 1; // initialize the frequency count to 1
-            node.value = value; // set the value of the item
-            node.key = key; // set the key of the item
-
-            if (counterMap[1] != null) { // if there are already items with frequency count 1
-                tails[1].next = node; // add the new item to the end of the linked list
-                node.previous = tails[1]; // set the previous node of the new item to the current tail
-                tails[1] = node; // update the tail of the linked list to the new item
-                node.next = null; // set the next node of the new item to null
-            } else {
-                // if this is the first item with frequency count 1
-                counterMap[1] = tails[1] = node;
-            }
-            // update the least frequent counter
-            leastCounter = 1;
-            // add the item to the cache
-            cache[key] = node;
-            // increment the size of the cache
-            size++;
+        if (cap <= 0) return; // if the capacity is 0, do nothing
+        if (vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key); // call get() to increment the use count for the key and update the LinkedHashSet
+            return;
         }
+        if (vals.size() >= cap) {
+            int evit = lists.get(min).iterator().next();
+            lists.get(min).remove(evit);
+            vals.remove(evit);
+            counts.remove(evit); // if the cache is full, remove the key with the minimum use count
+        }
+        vals.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key); // add the new key with a use count of 1
     }
-
-    private void incCounter(Node node) {
-        // Store the old count of the node
-        int oldCount = node.counter;
-
-        // Increment the counter of the node
-        int newCount = ++node.counter;
-
-        // Remove the node from the linked list of the old counter
-        if (node.previous != null) {
-            node.previous.next = node.next;
-        }
-
-        if (node.next != null) {
-            node.next.previous = node.previous;
-        }
-
-        // Move the pointer to the next element if it was the first element
-        if (node.previous == null) {
-            counterMap[oldCount] = node.next;
-        }
-        if (node.next == null) {
-            tails[oldCount] = node.previous;
-        }
-
-        // Check if there is only one element of such count
-        if (node.next == null && node.previous == null) {
-            if (oldCount == leastCounter) {
-                leastCounter = newCount;
-            } else {
-                leastCounter = Math.min(leastCounter, newCount);
-            }
-        } else {
-            leastCounter = Math.min(leastCounter, oldCount);
-        }
-
-        // Add the node to the new position in the linked list
-        if (counterMap[newCount] != null) {
-            tails[newCount].next = node;
-            node.previous = tails[newCount];
-            tails[newCount] = node;
-            node.next = null;
-        } else {
-            counterMap[newCount] = tails[newCount] = node;
-            node.next = null;
-            node.previous = null;
-        }
-    }
-
-    private void evict() {
-        // Retrieve the node with the least counter value
-        Node node = counterMap[leastCounter];
-        // Remove the node from the counterMap
-        counterMap[leastCounter] = node.next;
-
-        // If the node has a next node, update its previous pointer
-        if (node.next != null) {
-            node.next.previous = null;
-        } else {
-            // If the node does not have a next node, update the tail pointer for the counter
-            tails[leastCounter] = node.previous;
-        }
-        // Remove the node from the cache
-        cache[node.key] = null;
-        // Decrement the size of the cache
-        size--;
-    }
-
-    private static class Node {
-        // Pointers to the next and previous node in the linked list
-        Node next;
-        Node previous;
-        // The key and value for the node
-        int key;
-        int value;
-        // The counter for the node
-        int counter;
-    }
-
 }
