83148070a75c302f9de3b077f334fd5a1a44ee74
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-29 20:06:17 +0200

    1/29/2023

--- LC Difficulty/2. Medium/91. Decode Ways/A91.java
+++ LC Difficulty/2. Medium/91. Decode Ways/A91.java
@@ -1,7 +1,7 @@
 public class A91 {
     public static void main(String[] args) {
         B91 b91 = new B91();
-        String s = "226";
+        String s = "789";
         int result = b91.numDecodings(s);
         System.out.println(result);
     }
