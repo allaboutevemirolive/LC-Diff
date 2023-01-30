da2760cd813dbd69119e87bb7738f1b9a08c6628
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-30 18:09:20 +0200

    1/30/2023

--- LC Difficulty/2. Medium/93. Restore IP Addresses/B93.java
+++ LC Difficulty/2. Medium/93. Restore IP Addresses/B93.java
@@ -2,10 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class B93 {
+    // Wrapper function to return the result
+    public List<String> restoreIpAddresses(String s) {
+        // Create a list to store the result
+        ArrayList<String> result = new ArrayList<>();
+        // Call the recursive function to process the string
+        restoreIpAddresses(result, new char[s.length() + 3], 0, s, 0, 0);
+        // Return the result
+        return result;
+    }
+
     // Recursive function to restore IP addresses
     public void restoreIpAddresses(List<String> result, char[] buffer, int x, String s, int index, int number) {
         // If the number of integers (separated by dots) is 4, check if all characters in the string are processed
+        // Checks if the length of the current extracted IP address segment is 4 characters long
         if (number == 4) {
+            //  Check if the entire input string s has been processed 
             if (index == s.length()) {
                 // If yes, add the buffer containing IP address to result
                 result.add(new String(buffer));
@@ -16,29 +28,30 @@ public class B93 {
         int mem = 0;
         // Loop through up to 3 characters from the index in the string
         for (int i = 0; i < 3 && index + i < s.length(); i++) {
+            // Convert a sequence of digits in the input string s into an integer value
             mem = mem * 10 + s.charAt(index + i) - '0';
+            // Condition 1
             // If the integer value is larger than 255, break the loop
             if (mem > 255) break;
-            // Copy the processed character to the buffer
+            // Copies the current character from the input string s 
+            // to the buffer array at the position x + i
             buffer[x + i] = s.charAt(index + i);
-            // If there's room for the dot in the buffer, add it
+            // Check whether there is enough space in the buffer array 
+            // to store the next character
+            // The purpose of the expression x + i + 1 is to determine the next index in the buffer array 
+            // after the current character (which is stored at x + i) has been added
             if (x + i + 1 < buffer.length) {
+                // If there is enough room in the buffer array (i.e. x + i + 1 < buffer.length), 
+                // then a dot '.' is added after the character at the index x + i + 1
                 buffer[x + i + 1] = '.';
             }
             // Recursively call the function to process the next integer
             restoreIpAddresses(result, buffer, x + i + 2, s, index + i + 1, number + 1);
-            // If the integer value is 0, break the loop to prevent leading 0s
+            // Condition 2
+            // Checks if the computed integer value mem has leading zeros, 
+            // and if so, it stops the loop
             if (mem == 0) break;
         }
     }
 
-    // Wrapper function to return the result
-    public List<String> restoreIpAddresses(String s) {
-        // Create a list to store the result
-        ArrayList<String> result = new ArrayList<>();
-        // Call the recursive function to process the string
-        restoreIpAddresses(result, new char[s.length() + 3], 0, s, 0, 0);
-        // Return the result
-        return result;
-    }
 }
