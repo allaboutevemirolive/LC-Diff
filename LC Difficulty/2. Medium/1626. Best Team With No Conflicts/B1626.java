50b026678b0672ce3643cd5e2d7d624029a59cd2
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-02-02 00:19:33 +0200

    2/2/2023

--- LC Difficulty/2. Medium/1626. Best Team With No Conflicts/B1626.java
+++ LC Difficulty/2. Medium/1626. Best Team With No Conflicts/B1626.java
@@ -1,45 +1,64 @@
 import java.util.Arrays;
 
 class B1626 {
+    // This function returns the maximum score of a team that can be formed from given scores and ages of players
     public int bestTeamScore(int[] scores, int[] ages) {
 
+        // numPlayers is the number of players
         int numPlayers = scores.length;
+
+        // Create a 2D array 'players' where each player's score and age is stored
         int[][] players = new int[numPlayers][2];
 
+        // Store the score and age of each player in the 2D array
         for (int i = 0; i < numPlayers; i++) {
             players[i][0] = scores[i];
             players[i][1] = ages[i];
         }
 
+        // Sort the players based on their scores, in case of a tie, sort them based on their ages
         Arrays.sort(players, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
 
+        // maxAge is the maximum age among all players
         int maxAge = 0;
         for (int age : ages) {
             maxAge = Math.max(maxAge, age);
         }
 
+        // bitTree is the binary indexed tree used to store the scores
         int[] bitTree = new int[maxAge + 1];
+
+        // highestScore is the maximum score that can be achieved by a team
         int highestScore = 0;
 
+        // For each player
         for (int[] player : players) {
+            // currentScore is the score of the team when this player is included
             int currentScore = player[0] + queryBIT(bitTree, player[1]);
 
+            // Update the score of the team in the binary indexed tree
             updateBIT(bitTree, player[1], currentScore);
 
+            // Update the highestScore if the currentScore is greater
             highestScore = Math.max(highestScore, currentScore);
         }
+        // Return the highestScore
         return highestScore;
     }
 
+    // This function returns the maximum score of a team that can be formed from players with ages less than or equal to the given age
     private int queryBIT(int[] bitTree, int age) {
         int bestScore = Integer.MIN_VALUE;
+        // Traverse the binary indexed tree to find the maximum score
         for (int i = age; i > 0; i -= i & (-i)) {
             bestScore = Math.max(bestScore, bitTree[i]);
         }
         return bestScore;
     }
 
+    // This function updates the score of a team in the binary indexed tree
     private void updateBIT(int[] bitTree, int age, int currentScore) {
+        // Update the scores in the binary indexed tree
         for (int i = age; i < bitTree.length; i += i & (-i)) {
             bitTree[i] = Math.max(bitTree[i], currentScore);
         }
--- LC Difficulty/2. Medium/1626. Best Team With No Conflicts/C1626.java
+++ LC Difficulty/2. Medium/1626. Best Team With No Conflicts/C1626.java
@@ -2,24 +2,34 @@ import java.util.Arrays;
 
 class C1626 {
     public int bestTeamScore(int[] scores, int[] ages) {
-        int n = scores.length;
-        int[][] players = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            players[i][0] = ages[i];
-            players[i][1] = scores[i];
+        // number of players
+        int numberOfPlayers = scores.length;
+        // 2D array to store age and score of each player
+        int[][] playerDetails = new int[numberOfPlayers][2];
+        // storing player details into playerDetails array
+        for (int i = 0; i < numberOfPlayers; i++) {
+            playerDetails[i][0] = ages[i];
+            playerDetails[i][1] = scores[i];
         }
-        Arrays.sort(players, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        int[] dp = new int[n];
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i] = players[i][1];
+        // sorting the player details based on ages
+        Arrays.sort(playerDetails, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        // scores of selected players
+        int[] scoresOfSelectedPlayers = new int[numberOfPlayers];
+        // best team score
+        int bestTeamScore = 0;
+        // loop through all players
+        for (int i = 0; i < numberOfPlayers; i++) {
+            scoresOfSelectedPlayers[i] = playerDetails[i][1];
+            // loop through all previous players
             for (int j = 0; j < i; j++) {
-                if (players[j][1] <= players[i][1]) {
-                    dp[i] = Math.max(dp[i], dp[j] + players[i][1]);
+                // if previous player score is less than or equal to current player score
+                if (playerDetails[j][1] <= playerDetails[i][1]) {
+                    scoresOfSelectedPlayers[i] = Math.max(scoresOfSelectedPlayers[i], scoresOfSelectedPlayers[j] + playerDetails[i][1]);
                 }
             }
-            res = Math.max(res, dp[i]);
+            // update best team score
+            bestTeamScore = Math.max(bestTeamScore, scoresOfSelectedPlayers[i]);
         }
-        return res;
+        return bestTeamScore;
     }
 }
