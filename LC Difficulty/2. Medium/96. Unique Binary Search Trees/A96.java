6e579cfc306e11584546947b4b40a7845aebbf20
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-02-03 08:55:03 +0200

    2/3/2023

---
+++ LC Difficulty/2. Medium/96. Unique Binary Search Trees/A96.java
@@ -0,0 +1,7 @@
+public class A96 {
+    public static void main(String[] args) {
+        B96 solution = new B96();
+        int numOfNodes = 3;
+        System.out.println("The number of unique BST's with " + numOfNodes + " nodes is: " + solution.numTrees(numOfNodes));
+    }
+}
--- LC Difficulty/2. Medium/96. Unique Binary Search Trees/B96.java
+++ LC Difficulty/2. Medium/96. Unique Binary Search Trees/B96.java
@@ -1,14 +1,25 @@
 class B96 {
     public int numTrees(int n) {
+        // create an array to store the number of unique BST's with i nodes
         int[] dp = new int[n + 1];
+        
+        // base case: there is 1 unique BST with 0 or 1 node
         dp[0] = 1;
         dp[1] = 1;
+        
+        // loop through each number of nodes i
         for (int i = 2; i <= n; i++) {
+            // loop through each possible root j
             for (int j = 1; j <= i; j++) {
+                // the number of unique BST's with i nodes is the sum of
+                // the product of the number of BST's with j-1 nodes as the left subtree
+                // and the number of BST's with i-j nodes as the right subtree
                 dp[i] += dp[j - 1] * dp[i - j];
+                System.out.println(dp[i]);
             }
         }
+        
+        // return the number of BST's with n nodes
         return dp[n];
     }
-
 }
